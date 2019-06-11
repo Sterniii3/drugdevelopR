30a4cc77d88330b75d4b143da56757c83aab13eb
Preussler <lq149@imbi>
2019-06-11 15:39:44 +0200

    test print alpha beta

--- R/optimal_tte.R
+++ R/optimal_tte.R
@@ -158,6 +158,8 @@ optimal_tte <- function(w,  hr1, hr2, id1, id2,
 
     cl <-  makeCluster(getOption("cl.cores", num_cl)) #define cluster
 
+    print(c(alpha, beta))
+    
     clusterExport(cl, c("pmvnorm", "dmvnorm", "prior_tte", "Epgo_tte", "Ed3_tte",
                         "EPsProg_tte", "alpha", "beta",
                         "steps1", "steps2", "stepm1", "stepm2", "stepl1", "stepl2",
