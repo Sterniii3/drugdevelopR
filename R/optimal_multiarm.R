ce72a10948be2cd2a23090438ceaebc58e05583a
Preussler <lq149@imbi>
2019-07-02 15:30:45 +0200

    strategy

--- R/optimal_multiarm.R
+++ R/optimal_multiarm.R
@@ -27,7 +27,7 @@
 #' @param b1 expected gain for effect size category "small"
 #' @param b2 expected gain for effect size category "medium"
 #' @param b3 expected gain for effect size category "large"
-#' @param strategy choose Strategy: 1: "only best promising", 2: "all promising" or "both"
+#' @param strategy choose Strategy: 1 ("only best promising"), 2 ("all promising") or 3 (both)
 #' @param num_cl number of clusters used for parallel computing, default: 1
 #' @return
 #' The output of the function \code{\link{optimal_multiarm}} is a data.frame containing the optimization results:
@@ -61,7 +61,7 @@
 #'   stepm1 = 0.95,                                         # "medium"
 #'   stepl1 = 0.85,                                         # and "large" treatment effect size categories as proposed by IQWiG (2016)
 #'   b1 = 1000, b2 = 2000, b3 = 3000,                       # define expected benefit for a "small", "medium" and "large" treatment effect
-#'   strategy = 1,                                          # choose strategy
+#'   strategy = 1,                                          # choose strategy: 1, 2 or 3
 #'   num_cl = 1)                                            # set number of cores used for parallelized computing (check maximum number possible with detectCores())
 #' res
 #' cat(comment(res))                                        # displays the optimization sequence, start and finish date of the optimization procedure.
@@ -108,63 +108,70 @@ optimal_multiarm <- function(hr1, hr2, ec,
   HRGO <- seq(hrgomin, hrgomax, stephrgo)
   N2   <- seq(n2min, n2max, stepn2)
 
-  ufkt <- spfkt <- pgofkt <- K2fkt <- K3fkt <-
-   sp2fkt <- sp3fkt <- n3fkt <- matrix(0, length(N2), length(HRGO))
-
-  cat("Optimization progress:", fill = TRUE)
-  cat("", fill = TRUE)
-  pb <- txtProgressBar(min = 0, max = length(HRGO), style = 3, label = "Optimization progess")
-
-  for(j in 1:length(HRGO)){
-
-    HRgo <- HRGO[j]
-
-    cl <-  makeCluster(getOption("cl.cores", num_cl)) #define cluster
+  if(strategy==1){STRATEGY = 1}
+  if(strategy==2){STRATEGY = 2}
+  if(strategy==3){STRATEGY = c(1, 2)}
+  
+  result <- NULL
+  
+  for(strategy in STRATEGY){
+    ufkt <- spfkt <- pgofkt <- K2fkt <- K3fkt <-
+      sp2fkt <- sp3fkt <- n3fkt <- matrix(0, length(N2), length(HRGO))
     
-    clusterExport(cl, c("pmvnorm", "dmvnorm", "pgo", "ss", "Ess",
-                        "PsProg", "alpha", "beta",
-                        "steps1", "steps2", "stepm1", "stepm2", "stepl1", "stepl2",
-                        "K", "N", "S", 
-                        "c2", "c3", "c02", "c03",
-                        "b1", "b2", "b3", "HRgo",
-                        "hr1", "hr2", "ec"), envir = environment())
-
+    cat("Optimization progress:", fill = TRUE)
+    cat("", fill = TRUE)
+    pb <- txtProgressBar(min = 0, max = length(HRGO), style = 3, label = "Optimization progess")
     
-    result <- parSapply(cl, N2, utility_multiarm, HRgo,
-                        alpha,beta,hr1,hr2,
-                        strategy,ec,c2,c02,c3,c03,
-                        K,N,S,
-                        steps1,stepm1,stepl1,
-                        b1, b2, b3)
-
-    setTxtProgressBar(title= "i", pb, j)
-    stopCluster(cl)
-
-    ufkt[, j]     <-  result[1, ]
-    n3fkt[, j]    <-  result[2, ]
-    spfkt[, j]    <-  result[3, ]
-    pgofkt[, j]   <-  result[4, ]
-    sp2fkt[, j]   <-  result[5, ]
-    sp3fkt[, j]   <-  result[6, ]
-    K2fkt[, j]    <-  result[7, ]
-    K3fkt[, j]    <-  result[8, ]
-   }
-
-  ind   <-  which(ufkt  ==  max(ufkt), arr.ind <-  TRUE)
-
-  I <-  as.vector(ind[1, 1])
-  J <-  as.vector(ind[1, 2])
-
-  Eud   <- ufkt[I, J]
-  n3    <- n3fkt[I, J]
-  prob  <- spfkt[I, J]
-  pg    <- pgofkt[I, J]
-  k2    <- K2fkt[I, J]
-  k3    <- K3fkt[I, J]
-  prob2 <- sp2fkt[I, J]
-  prob3 <- sp3fkt[I, J]
+    for(j in 1:length(HRGO)){
+      
+      HRgo <- HRGO[j]
+      
+      cl <-  makeCluster(getOption("cl.cores", num_cl)) #define cluster
+      
+      clusterExport(cl, c("pmvnorm", "dmvnorm","adaptIntegrate", "pgo", "ss", "Ess",
+                          "PsProg", "alpha", "beta",
+                          "steps1", "steps2", "stepm1", "stepm2", "stepl1", "stepl2",
+                          "K", "N", "S", 
+                          "c2", "c3", "c02", "c03",
+                          "b1", "b2", "b3", "HRgo",
+                          "hr1", "hr2", "ec"), envir = environment())
+      
+      
+      res <- parSapply(cl, N2, utility_multiarm, HRgo,
+                          alpha,beta,hr1,hr2,
+                          strategy,ec,c2,c02,c3,c03,
+                          K,N,S,
+                          steps1,stepm1,stepl1,
+                          b1, b2, b3)
+      
+      setTxtProgressBar(title= "i", pb, j)
+      stopCluster(cl)
+      
+      ufkt[, j]     <-  res[1, ]
+      n3fkt[, j]    <-  res[2, ]
+      spfkt[, j]    <-  res[3, ]
+      pgofkt[, j]   <-  res[4, ]
+      sp2fkt[, j]   <-  res[5, ]
+      sp3fkt[, j]   <-  res[6, ]
+      K2fkt[, j]    <-  res[7, ]
+      K3fkt[, j]    <-  res[8, ]
+    }
     
-  result <-  data.frame(u = round(Eud,2), HRgo = HRGO[J], n2 = N2[I], n3 = n3, n = n2 + n3,
+    ind   <-  which(ufkt  ==  max(ufkt), arr.ind <-  TRUE)
+    
+    I <-  as.vector(ind[1, 1])
+    J <-  as.vector(ind[1, 2])
+    
+    Eud   <- ufkt[I, J]
+    n3    <- n3fkt[I, J]
+    prob  <- spfkt[I, J]
+    pg    <- pgofkt[I, J]
+    k2    <- K2fkt[I, J]
+    k3    <- K3fkt[I, J]
+    prob2 <- sp2fkt[I, J]
+    prob3 <- sp3fkt[I, J]
+    
+    result <-  rbind(result, data.frame(u = round(Eud,2), HRgo = HRGO[J], n2 = N2[I], n3 = n3, n = n2 + n3,
                           pgo = round(pg,2), sProg = round(prob,2),
                           hr1 = hr1, hr2 = hr2, ec = ec,
                           K = K, N = N, S = S, K2 = round(k2), K3 = round(k3),
@@ -172,7 +179,9 @@ optimal_multiarm <- function(hr1, hr2, ec,
                           steps1 = round(steps1,2), stepm1 = round(stepm1,2), stepl1 = round(stepl1,2),
                           alpha = alpha, beta = beta, 
                           c02 = c02, c03 = c03, c2 = c2, c3 = c3, 
-                          b1 = b1, b2 = b2, b3 = b3)
+                          b1 = b1, b2 = b2, b3 = b3))  
+  }
+
 
   comment(result) <-   c("\noptimization sequence HRgo:", HRGO,
                     "\noptimization sequence n2:", N2,
