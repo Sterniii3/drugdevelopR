621bb23542adebddd9bc150b70a5b79a17e269fe
johannescep <89907539+johannescep@users.noreply.github.com>
2022-03-22 18:28:54 +0100

    Update optimal_multiarm_binary.R
    deleting ec, adding informations

--- R/optimal_multiarm_binary.R
+++ R/optimal_multiarm_binary.R
@@ -1,4 +1,91 @@
-optimal_multiarm_binary <- function(p0, p11, p12, ec,
+#' Optimal phase II/III drug development planning with binary endpoint
+#'
+#' The drugdevelopR package enables planning of phase II/III drug development programs with optimal sample size allocation and go/no-go decision rules. For binary endpoints the treatment effect is measured by the risk ratio (RR).The assumed true treatment effects can be assumed fixed or modelled by a prior distribution. The R Shiny application \href{https://web.imbi.uni-heidelberg.de/prior/}{prior} visualizes the prior distributions used in this package. Fast coputing is enabled by parallel programming.
+#' 
+#' @name optimal_multiarm_binary
+#' @param p0 assumed true rate of control group
+#' @param p11 assumed true rate of treatment group
+#' @param p12 assumed true rate of treatment group
+#' @param n2min minimal total sample size for phase II; must be even number
+#' @param n2max maximal total sample size for phase II, must be even number
+#' @param stepn2 stepsize for the optimization over n2; must be even number
+#' @param rrgomin minimal threshold value for the go/no-go decision rule
+#' @param rrgomax maximal threshold value for the go/no-go decision rule
+#' @param steprrgo stepsize for the optimization over RRgo
+#' @param beta 1-beta power for calculation of sample size for phase III
+#' @param alpha significance level
+#' @param c2 variable per-patient cost for phase II
+#' @param c3 variable per-patient cost for phase III
+#' @param c02 fixed cost for phase II
+#' @param c03 fixed cost for phase III
+#' @param K constraint on the costs of the program, default: Inf, e.g. no constraint
+#' @param N constraint on the total expected sample size of the program, default: Inf, e.g. no constraint
+#' @param S constraint on the expected probability of a successful program, default: -Inf, e.g. no constraint
+#' @param steps1 lower boundary for effect size category "small" in RR scale, default: 1
+#' @param stepm1 lower boundary for effect size category "medium" in RR scale = upper boundary for effect size category "small" in RR scale, default: 0.95
+#' @param stepl1 lower boundary for effect size category "large" in RR scale = upper boundary for effect size category "medium" in RR scale, default: 0.85
+#' @param b1 expected gain for effect size category "small"
+#' @param b2 expected gain for effect size category "medium"
+#' @param b3 expected gain for effect size category "large"
+#' @param num_cl number of clusters used for parallel computing, default: 1
+#' @return
+#' The output of the function \code{\link{optimal_binary}} is a data.frame containing the optimization results:
+#' \describe{
+#'   \item{u}{maximal expected utility}
+#'   \item{RRgo}{optimal threshold value for the decision rule to go to phase III}
+#'   \item{n2}{total sample size for phase II}
+#'   \item{n3}{total sample size for phase III; rounded to the next even natural number}
+#'   \item{n}{total sample size in the program; n = n2 + n3}
+#'   \item{K}{maximal costs of the program}
+#'   \item{pgo}{probability to go to phase III}
+#'   \item{sProg}{probability of a successful program}
+#'   \item{sProg1}{probability of a successful program with "small" treatment effect in Phase III}
+#'   \item{sProg2}{probability of a successful program with "medium" treatment effect in Phase III}
+#'   \item{sProg3}{probability of a successful program with "large" treatment effect in Phase III }
+#'   \item{K2}{expected costs for phase II}
+#'   \item{K3}{expected costs for phase III}
+#' }
+#' and further input parameters.
+#'
+#' Taking cat(comment()) of the data.frame object lists the used optimization sequences, start and finish date of the optimization procedure.
+#' @examples
+#' res <- optimal_multiarm_binary( p0 = 0.6, p11 =  0.3, p12 = 0.5, 
+#'   n2min = 20, n2max = 100, stepn2 = 4,                   # define optimization set for n2
+#'   rrgomin = 0.7, rrgomax = 0.9, steprrgo = 0.05,         # define optimization set for RRgo
+#'   alpha = 0.05, beta = 0.1,                              # drug development planning parameters
+#'   c2 = 0.75, c3 = 1, c02 = 100, c03 = 150,               # define fixed and variable costs for phase II and III,
+#'   K = Inf, N = Inf, S = -Inf,                            # set maximal costs/ expected sample size for the program or minimal expected probability of a successful program
+#'   steps1 = 1,                                            # define lower boundary for "small"
+#'   stepm1 = 0.95,                                         # "medium"
+#'   stepl1 = 0.85,                                         # and "large" treatment effect size categories as proposed by IQWiG (2016)
+#'   b1 = 1000, b2 = 2000, b3 = 3000,                       # define expected benefit for a "small", "medium" and "large" treatment effect
+#'   strategy = 1, num_cl = 1)                              # set number of cores used for parallelized computing (check maximum number possible with detectCores())
+#' res
+#' cat(comment(res))                                        # displays the optimization sequence, start and finish date of the optimization procedure.
+#' @section drugdevelopR functions:
+#' The drugdevelopR package provides the functions
+#' \itemize{
+#'   \item \code{\link{optimal_tte}},
+#'   \item \code{\link{optimal_binary}} or
+#'   \item \code{\link{optimal_normal}}
+#' }
+#' to plan optimal phase II/III drug development programs with
+#' \itemize{
+#'   \item time-to-event (treatment effect measured by hazard ratio (HR)),
+#'   \item binary (treatment effect measured by risk ratio (RR)) and
+#'   \item normally distributed (treatment effect measured by standardized difference in means (Delta))
+#' }
+#' endpoint, where the treatment effect is modelled by a \href{https://web.imbi.uni-heidelberg.de/prior/}{prior}. Optimal phase II/III drug development planning with fixed treatment effects can be done with the help of the R Shiny application \href{https://web.imbi.uni-heidelberg.de/basic/}{basic}. Extensions are 
+#' \itemize{
+#'   \item optimal planning of programs including methods for discounting of phase II results (function: \code{\link{optimal_bias}}, App: \href{https://web.imbi.uni-heidelberg.de/bias/}{bias}),
+#'   \item optimal planning of programs with several phase III trials (function: \code{\link{optimal_multitrial}}, App: \href{https://web.imbi.uni-heidelberg.de/multitrial/}{multitrial}) and
+#'   \item optimal planning of programs with multiple arms (function: \code{\link{optimal_multiarm}}, App: \href{https://web.imbi.uni-heidelberg.de/multiarm/}{multiarm}).
+#' }
+#' @references
+#' IQWiG (2016). Allgemeine Methoden. Version 5.0, 10.07.2016, Technical Report. Available at \href{https://www.iqwig.de/de/methoden/methodenpapier.3020.html}{https://www.iqwig.de/de/methoden/methodenpapier.3020.html}, assessed last 15.05.19.
+#' @export
+
+optimal_multiarm_binary <- function(p0, p11, p12, 
                              n2min, n2max, stepn2,
                              rrgomin, rrgomax, steprrgo,
                              alpha, beta,
@@ -44,11 +131,11 @@ optimal_multiarm_binary <- function(p0, p11, p12, ec,
                           "K", "N", "S", "strategy",
                           "c2", "c3", "c02", "c03",
                           "b1", "b2", "b3", "RRgo",
-                          "p0", "p11", "p12", "ec"), envir = environment())
+                          "p0", "p11", "p12"), envir = environment())
       
       
       res <- parSapply(cl, N2, utility_multiarm_binary, RRgo,
-                       alpha,beta,p0,p11,p12,strategy,ec,
+                       alpha,beta,p0,p11,p12,strategy,
                        c2,c02,c3,c03,K,N,S,
                        steps1, stepm1, stepl1,b1, b2, b3)
       
@@ -82,7 +169,7 @@ optimal_multiarm_binary <- function(p0, p11, p12, ec,
     result <-  rbind(result, data.frame(Strategy = strategy,u = round(Eud,2), RRgo = RRGO[J], n2 = N2[I], 
                                         n3 = n3, n = N2[I] + n3,
                                         pgo = round(pg,2), sProg = round(prob,2),
-                                        p0 = p0, p11 = p11, p12 = p12, ec = ec,
+                                        p0 = p0, p11 = p11, p12 = p12,
                                         K = K, N = N, S = S, K2 = round(k2), K3 = round(k3),
                                         sProg2 = round(prob2,2), sProg3 = round(prob3,2),
                                         steps1 = round(steps1,2), stepm1 = round(stepm1,2), stepl1 = round(stepl1,2),
