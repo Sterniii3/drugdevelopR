257126e436ffea3c7e359ee9cd1ece0244433a6b
Johannes Cepicka <j36.cepicka@gmail.com>
2022-07-07 01:08:41 +0200

    writing some test for functions_normal

--- tests/testthat/test-functions_normal.R
+++ tests/testthat/test-functions_normal.R
@@ -23,3 +23,48 @@
                                      a=0.25,b=0.75, fixed=TRUE),
                          pnorm((0.625-0.2)/sqrt(4/50),0,1)           )
  })
+ 
+ 
+ test_that("Higher Treatment effect reduces n3",{
+            expect_gt(En3_normal(kappa=0.2,n2=50,w=0.3,
+                                 alpha = 0.05, beta = 0.1,
+                                 Delta1=0.625, Delta2=0.325, 
+                                 in1=300, in2=600, 
+                                 a=0.25,b=0.75, fixed=TRUE),
+                      En3_normal(kappa=0.2,n2=50,w=0.3,
+                                 alpha = 0.05, beta = 0.1,
+                                 Delta1=0.825, Delta2=0.325, 
+                                 in1=300, in2=600, 
+                                 a=0.25,b=0.75, fixed=TRUE))
+ })
+ 
+ 
+ test_that("Higher Treatment effect increases probability of a success",{
+     expect_lt(EPsProg_normal(kappa=0.2,n2=50,w=0.3,
+                          alpha = 0.05, beta = 0.1,
+                          step1 = 0.5, step2 = 1,
+                          Delta1=0.625, Delta2=0.325, 
+                          in1=300, in2=600, 
+                          a=0.25,b=0.75, fixed=TRUE, gamma=0),
+               EPsProg_normal(kappa=0.2,n2=50,w=0.3,
+                          alpha = 0.05, beta = 0.1,
+                          step1 = 0.5, step2 = 1,
+                          Delta1=0.825, Delta2=0.325, 
+                          in1=300, in2=600, 
+                          a=0.25,b=0.75, fixed=TRUE, gamma=0))
+ })
+ 
+ test_that("Bigger success region increases probability of a success",{
+     expect_lt(EPsProg_normal(kappa=0.2,n2=50,w=0.3,
+                              alpha = 0.05, beta = 0.1,
+                              step1 = 0.5, step2 = 1,
+                              Delta1=0.625, Delta2=0.325, 
+                              in1=300, in2=600, 
+                              a=0.25,b=0.75, fixed=TRUE, gamma=0),
+               EPsProg_normal(kappa=0.2,n2=50,w=0.3,
+                              alpha = 0.05, beta = 0.1,
+                              step1 = 0.3, step2 = 1,
+                              Delta1=0.825, Delta2=0.325, 
+                              in1=300, in2=600, 
+                              a=0.25,b=0.75, fixed=TRUE, gamma=0))
+ })
