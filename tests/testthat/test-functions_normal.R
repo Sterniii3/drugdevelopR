1bd28cabccce20fc2eb8f1a6a7167bb9f23997dc
Johannes Cepicka <j36.cepicka@gmail.com>
2022-08-09 10:40:46 +0200

    writing tests for functions_normal

--- tests/testthat/test-functions_normal.R
+++ tests/testthat/test-functions_normal.R
@@ -1,3 +1,23 @@
+test_that("prior_normal returns the same results", {
+  expect_equal(prior_normal(x=0.5,w=0.3,Delta1=1.5,Delta2=0.95,
+                            in1=200,in2=400,a=0.4,b=0.6),
+               0.81469965)
+})
+
+test_that("expanding boundaries reduces value", {
+  expect_gt(prior_normal(x=0.5,w=0.3,Delta1=1.5,Delta2=0.95,
+                            in1=200,in2=400,a=0.4,b=0.6),
+               prior_normal(x=0.5,w=0.3,Delta1=1.5,Delta2=0.95,
+                            in1=200,in2=400,a=0.2,b=0.8))
+})
+
+test_that("box normal returns a vector with 100000 realizations", {
+  expect_length(box_normal(w=0.3,Delta1=1.5,Delta2=0.95,
+                           in1=200,in2=400,a=0.4,b=0.6), 1000000)
+})
+
+
+
  test_that("Epgo_normal returns the same results", {
      expect_equal(Epgo_normal(kappa=0.2,n2=50,w=0.3,
                               Delta1=0.625, Delta2=0.325, 
@@ -67,4 +87,119 @@
                               Delta1=0.825, Delta2=0.325, 
                               in1=300, in2=600, 
                               a=0.25,b=0.75, fixed=TRUE, gamma=0))
+ })
+ 
+ test_that("Higher gains for each benefit category increase utility",{
+    expect_lt(getElement(utility_normal(n2=60, kappa=0.1, w=0.3, Delta1 = 0.375, Delta2 = 0.625, 
+                             in1=300, in2=600, a = 0.25, b = 0.75,
+                             alpha = 0.05, beta = 0.1, 
+                             c2 = 0.675, c3 = 0.72, c02 = 15, c03 = 20, 
+                             K=Inf, N=Inf, S=-Inf,
+                             steps1=0, stepm1=0.5, stepl1=0.8,
+                             b1=1000, b2=2000, b3=3000,
+                             gamma=0, fixed=TRUE),1),
+              getElement(utility_normal(n2=60, kappa=0.1, w=0.3, Delta1 = 0.375, Delta2 = 0.625, 
+                             in1=300, in2=600, a = 0.25, b = 0.75,
+                             alpha = 0.05, beta = 0.1, 
+                             c2 = 0.675, c3 = 0.72, c02 = 15, c03 = 20, 
+                             K=Inf, N=Inf, S=-Inf,
+                             steps1=0, stepm1=0.5, stepl1=0.8,
+                             b1=2000, b2=4000, b3=6000,
+                             gamma=0, fixed=TRUE),1))
+ })
+ 
+ 
+ test_that("Lower treatment effects decrease utility",{
+   expect_gt(getElement(utility_normal(n2=60, kappa=0.1, w=0.3, Delta1 = 0.675, Delta2 = 0.825, 
+                                       in1=300, in2=600, a = 0.25, b = 0.75,
+                                       alpha = 0.05, beta = 0.1, 
+                                       c2 = 0.675, c3 = 0.72, c02 = 15, c03 = 20, 
+                                       K=Inf, N=Inf, S=-Inf,
+                                       steps1=0, stepm1=0.5, stepl1=0.8,
+                                       b1=1000, b2=2000, b3=3000,
+                                       gamma=0, fixed=FALSE),1),
+             getElement(utility_normal(n2=60, kappa=0.1, w=0.3, Delta1 = 0.375, Delta2 = 0.625, 
+                                       in1=300, in2=600, a = 0.25, b = 0.75,
+                                       alpha = 0.05, beta = 0.1, 
+                                       c2 = 0.675, c3 = 0.72, c02 = 15, c03 = 20, 
+                                       K=Inf, N=Inf, S=-Inf,
+                                       steps1=0, stepm1=0.5, stepl1=0.8,
+                                       b1=1000, b2=2000, b3=3000,
+                                       gamma=0, fixed=FALSE),1))
+ })
+ 
+ test_that("Higher costs decrease utility",{
+   expect_gt(getElement(utility_normal(n2=60, kappa=0.1, w=0.3, Delta1 = 0.375, Delta2 = 0.625, 
+                                       in1=300, in2=600, a = 0.25, b = 0.75,
+                                       alpha = 0.05, beta = 0.1, 
+                                       c2 = 0.675, c3 = 0.72, c02 = 15, c03 = 20, 
+                                       K=Inf, N=Inf, S=-Inf,
+                                       steps1=0, stepm1=0.5, stepl1=0.8,
+                                       b1=1000, b2=2000, b3=3000,
+                                       gamma=0, fixed=FALSE),1),
+             getElement(utility_normal(n2=60, kappa=0.1, w=0.3, Delta1 = 0.375, Delta2 = 0.625, 
+                                       in1=300, in2=600, a = 0.25, b = 0.75,
+                                       alpha = 0.05, beta = 0.1, 
+                                       c2 = 0.8, c3 = 0.75, c02 = 20, c03 = 25, 
+                                       K=Inf, N=Inf, S=-Inf,
+                                       steps1=0, stepm1=0.5, stepl1=0.8,
+                                       b1=1000, b2=2000, b3=3000,
+                                       gamma=0, fixed=FALSE),1))
+ })
+ 
+ test_that("n3_skipII_normal returns same results",{
+   expect_equal(n3_skipII_normal(alpha=0.05,beta=0.1,median_prior=0.675),
+                75.183296)
+ })
+ 
+ test_that("Higher treatment effect (i.e median prior larger) leads to smaller number of n3",{
+   expect_gt(n3_skipII_normal(alpha=0.05,beta=0.1,median_prior=0.675),
+             n3_skipII_normal(alpha=0.05,beta=0.1,median_prior=0.875))
+ })
+ 
+ test_that("Higher Treatment effect increases probability of a success",{
+   expect_lt(EPsProg_skipII_normal(alpha = 0.05, beta = 0.1,
+                            median_prior = 0.625, step1 = 0.5, step2 = 1,
+                            Delta1=0.625, Delta2=0.325, 
+                            in1=300, in2=600, 
+                            a=0.25,b=0.75, fixed=TRUE, gamma=0),
+             EPsProg_skipII_normal(alpha = 0.05, beta = 0.1,
+                            median_prior = 0.625,step1 = 0.5, step2 = 1,
+                            Delta1=0.825, Delta2=0.325, 
+                            in1=300, in2=600, 
+                            a=0.25,b=0.75, fixed=TRUE, gamma=0))
+ }) 
+ 
+ test_that("Utility decreases with higher costs",{
+   expect_gt(utility_skipII_normal(alpha = 0.05, beta = 0.1, median_prior = 0.625,
+                                   c03 = 0.72, c3=20, b1=1000, b2=2000, b3=3000,  
+                                   K=Inf, N=Inf, S=-Inf,
+                                   steps1=0, stepm1=0.5, stepl1=0.8,
+                                   w=0.3, Delta1=0.625, Delta2=0.325,
+                                   in1=300, in2=600,a=0.25,b=0.75, 
+                                   fixed=TRUE, gamma=0)[1],
+             utility_skipII_normal(alpha = 0.05, beta = 0.1, median_prior = 0.625,
+                                   c03 = 0.75, c3=25, b1=1000, b2=2000, b3=3000,  
+                                   K=Inf, N=Inf, S=-Inf,
+                                   steps1=0, stepm1=0.5, stepl1=0.8,
+                                   w=0.3, Delta1=0.625, Delta2=0.325,
+                                   in1=300, in2=600,a=0.25,b=0.75, 
+                                   fixed=TRUE, gamma=0)[1])
+ })
+ 
+ test_that("Utility increases with higher gains",{
+   expect_lt(utility_skipII_normal(alpha = 0.05, beta = 0.1, median_prior = 0.625,
+                                   c03 = 0.72, c3=20, b1=1000, b2=2000, b3=3000,  
+                                   K=Inf, N=Inf, S=-Inf,
+                                   steps1=0, stepm1=0.5, stepl1=0.8,
+                                   w=0.3, Delta1=0.625, Delta2=0.325,
+                                   in1=300, in2=600,a=0.25,b=0.75, 
+                                   fixed=TRUE, gamma=0)[1],
+             utility_skipII_normal(alpha = 0.05, beta = 0.1, median_prior = 0.625,
+                                   c03 = 0.72, c3=20, b1=2000, b2=4000, b3=6000,  
+                                   K=Inf, N=Inf, S=-Inf,
+                                   steps1=0, stepm1=0.5, stepl1=0.8,
+                                   w=0.3, Delta1=0.625, Delta2=0.325,
+                                   in1=300, in2=600,a=0.25,b=0.75, 
+                                   fixed=TRUE, gamma=0)[1])
  })
