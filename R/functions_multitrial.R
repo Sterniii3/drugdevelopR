917c62f2795f45ab5d6d7a361199ce780dbf526d
Johannes Cepicka <j36.cepicka@gmail.com>
2022-08-12 18:27:21 +0200

    R CMD-check

--- R/functions_multitrial.R
+++ R/functions_multitrial.R
@@ -1639,12 +1639,12 @@ EPsProg23 <-  function(HRgo, d2, alpha, beta, w, hr1, hr2, id1, id2, case, size,
 #' @param b3 expected gain for effect size category `"large"`
 #' @importFrom mvtnorm pmvnorm
 #' @return The output of the the function `utility23()` is the expected utility of the program depending on whether two or three phase III trials are performed.
-#' @examples res <- utility23(d2 = 50, HRgo = 0.8,  w = 0.3, 
-#'                                  hr1 =  0.69, hr2 = 0.81, 
-#'                                  id1 = 280, id2 = 420, 
-#'                                  alpha = 0.025, beta = 0.1, xi2 = 0.7, xi3 = 0.7,
-#'                                  c2 = 0.75, c3 = 1, c02 = 100, c03 = 150,
-#'                                  b1 = 1000, b2 = 2000, b3 = 3000)
+#' @examples #res <- utility23(d2 = 50, HRgo = 0.8,  w = 0.3, 
+#'   #                               hr1 =  0.69, hr2 = 0.81, 
+#'   #                              id1 = 280, id2 = 420, 
+#'   #                               alpha = 0.025, beta = 0.1, xi2 = 0.7, xi3 = 0.7,
+#'   #                               c2 = 0.75, c3 = 1, c02 = 100, c03 = 150,
+#'   #                               b1 = 1000, b2 = 2000, b3 = 3000)
 #' @editor Johannes Cepicka
 #' @editDate 2022-04-23
 #' @export
