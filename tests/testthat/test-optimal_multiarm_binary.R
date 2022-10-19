b8d204c9b3991228c736d1a3ff60ed9f3efce344
Johannes Cepicka <j36.cepicka@gmail.com>
2022-10-19 14:37:09 +0200

    writing some tests

---
+++ tests/testthat/test-optimal_multiarm_binary.R
@@ -0,0 +1,11 @@
+test_that("multiplication works", {
+  expect_equal(optimal_multiarm_binary( p0 = 0.6, p11 =  0.3, p12 = 0.5,
+                                        n2min = 20, n2max = 100, stepn2 = 4, 
+                                        rrgomin = 0.7, rrgomax = 0.9, steprrgo = 0.05,
+                                        alpha = 0.05, beta = 0.1, 
+                                        c2 = 0.75, c3 = 1, c02 = 100, c03 = 150,
+                                        K = Inf, N = Inf, S = -Inf,
+                                        steps1 = 1, stepm1 = 0.95, stepl1 = 0.85,
+                                        b1 = 1000, b2 = 2000, b3 = 3000,
+                                        strategy = 1, num_cl = 2)$u, 1671.27)
+})
---
+++ tests/testthat/test-optimal_multiarm_normal.R
@@ -0,0 +1,12 @@
+test_that("optimal_multiarm_normal works for strategy 1", {
+  expect_equal(optimal_multiarm_normal(Delta1 = 0.375, Delta2 = 0.625, 
+                                       n2min = 20, n2max = 100, stepn2 = 4,    
+                                       kappamin = 0.02, kappamax = 0.2, stepkappa = 0.02,    
+                                       alpha = 0.05, beta = 0.1,    
+                                       c2 = 0.675, c3 = 0.72, c02 = 15, c03 = 20,   
+                                       K = Inf, N = Inf, S = -Inf,   
+                                       steps1 = 0, stepm1 = 0.5, stepl1 = 0.8, 
+                                       b1 = 3000, b2 = 8000, b3 = 10000,   
+                                       strategy = 1, num_cl = 2)$u, 2924.13)
+})
+
