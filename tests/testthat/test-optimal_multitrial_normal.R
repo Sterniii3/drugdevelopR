bc33ca11293e4488c2c6e8059a129ce22c1f674e
Johannes Cepicka <j36.cepicka@gmail.com>
2022-10-24 14:46:51 +0200

    trying to solve error code 137

--- tests/testthat/test-optimal_multitrial_normal.R
+++ tests/testthat/test-optimal_multitrial_normal.R
@@ -1,4 +1,4 @@
-test_that("optimal_multitrial_normal works for case 1", {
+'test_that("optimal_multitrial_normal works for case 1", {
   expect_equal(optimal_multitrial_normal(w=0.3, Delta1 = 0.375, Delta2 = 0.625,
                                          in1=300, in2=600, a = 0.25, b = 0.75,
                                          n2min = 20, n2max = 100,stepn2 = 4,
@@ -9,4 +9,4 @@ test_that("optimal_multitrial_normal works for case 1", {
                                          b1 = 3000, b2 = 8000, b3 = 10000,
                                          case = 1, strategy = TRUE,
                                          fixed = TRUE,num_cl = 2)$u, c(1890.94,1928.79))
-})
+})'
