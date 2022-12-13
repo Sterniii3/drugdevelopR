396b8236825441639d9caf57d4c487cf73c39c02
Lukas D. Sauer <sauer@imbi.uni-heidelberg.de>
2022-12-13 17:20:18 +0100

    Test code 02.05

--- validation/test_code/02_BiasAdjustmentTestCode.R
+++ validation/test_code/02_BiasAdjustmentTestCode.R
@@ -153,5 +153,59 @@ test_that("02.04", {
   expect_equal(res[4,]$u, 77.40, tolerance = 0.0001)
   expect_equal(res[4,]$Adj, 0.475)
 })
-
+#' @editor Lukas D Sauer
+#' @editDate 2022-12-13
+test_that("02.05", {
+  # Without and with cost constraint
+  res <- optimal_bias(alpha = 0.025,
+                      beta = 0.1,
+                      hr1 = 0.69, hr2 = 0.88,
+                      xi2 = 0.7, xi3 = 0.7,
+                      d2min = 20, d2max = 100, stepd2 = 5,
+                      hrgomin = 0.7, hrgomax = 0.9, stephrgo = 0.02,
+                      steps1 = 1, stepm1 = 0.95, stepl1 = 0.85,
+                      b1 = 1000, b2 = 2000, b3 = 3000,
+                      num_cl = 3,
+                      c02 = 100, c03 = 150, # fixed cost
+                      c2 = 0.75, c3 = 1, # variable per patient cost
+                      fixed = TRUE,
+                      w = 0.3,
+                      id1 = 210, id2 = 420,
+                      adj = "additive",
+                      alphaCImin = 0.3, alphaCImax = 0.5, stepalphaCI = 0.025,
+                      lambdamin = NULL, lambdamax = NULL, steplambda = NULL
+  )
+  expect_equal(res$n2, 144)
+  expect_equal(res$n3, 456)
+  expect_equal(res$u, 853.02)
+  expect_equal(res$Adj, 0.475)
+  expect_equal(res$K2, 208)
+  expect_equal(res$K3, 582)
+  res <- optimal_bias(alpha = 0.025,
+                      beta = 0.1,
+                      hr1 = 0.69, hr2 = 0.88,
+                      xi2 = 0.7, xi3 = 0.7,
+                      d2min = 20, d2max = 100, stepd2 = 5,
+                      hrgomin = 0.7, hrgomax = 0.9, stephrgo = 0.02,
+                      steps1 = 1, stepm1 = 0.95, stepl1 = 0.85,
+                      b1 = 1000, b2 = 2000, b3 = 3000,
+                      num_cl = 3,
+                      c02 = 100, c03 = 150, # fixed cost
+                      c2 = 0.75, c3 = 1, # variable per patient cost
+                      fixed = TRUE,
+                      w = 0.3,
+                      id1 = 210, id2 = 420,
+                      adj = "additive",
+                      alphaCImin = 0.3, alphaCImax = 0.5, stepalphaCI = 0.025,
+                      lambdamin = NULL, lambdamax = NULL, steplambda = NULL,
+                      K = 400
+  )
+  expect_equal(res$n2, 44)
+  expect_equal(res$n3, 172)
+  expect_equal(res$u, 474.18)
+  expect_equal(res$Adj, 0.475)
+  expect_equal(res$K2, 133)
+  expect_equal(res$K3, 263)
+  
+})
 
