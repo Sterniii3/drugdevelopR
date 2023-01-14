105c8067c154a901e3ef7203dccfbceef8c5e007
Johannes Cepicka <j36.cepicka@gmail.com>
2023-01-14 14:51:09 +0100

    trying to solve parallelization issue

--- R/optimal_multiple_tte.R
+++ R/optimal_multiple_tte.R
@@ -116,8 +116,9 @@ result <- NULL
     res_test <- parallel::parSapply(cl,N2,pgo_multiple_tte, HRgo, hr1, hr2,
                                     id1, id2, fixed, rho)
     
-    res_test2 <- parallel::parSapply(cl, N2, Ess_multiple_tte, HRgo, alpha, beta,
-                                     hr1, hr2, id1, id2, fixed, rho)
+#    res_test2 <- parallel::parSapply(cl, N2, Ess_multiple_tte, HRgo, alpha, beta,
+#                                     hr1, hr2, id1, id2, fixed, rho)
+    res_test3 <- parallel::parSapply(cl, N2, pw, hr1,hr2,id1,id2,fixed,rho)
     
     res <- parallel::parSapply(cl, N2, utility_multiple_tte, HRgo,
                      alpha,beta,hr1,hr2,id1,id2,rho,fixed,
