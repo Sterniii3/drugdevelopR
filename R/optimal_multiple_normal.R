dd2da7e1b91f7879cf35c771630d38fa661d5b9b
Johannes Cepicka <j36.cepicka@gmail.com>
2023-01-24 14:45:06 +0100

    last try for parallelization

--- R/optimal_multiple_normal.R
+++ R/optimal_multiple_normal.R
@@ -106,7 +106,7 @@ optimal_multiple_normal <- function(Delta1, Delta2, in1, in2, sigma1, sigma2,
       cl <-  parallel::makeCluster(getOption("cl.cores", num_cl)) #define cluster
       
       parallel::clusterExport(cl, c("pmvnorm", "pnorm", "dmvnorm", "dnorm","qmvnorm", "qnorm","adaptIntegrate",
-                          "dbivanorm", "pgo_multiple_normal", "Ess_multiple_normal",
+                          "dbivanorm", "max", "min", "pgo_multiple_normal", "Ess_multiple_normal",
                           "EPsProg_multiple_normal", "posp_normal", "fmin", "alpha", "beta",
                           "steps1", "stepm1", "stepl1",
                           "K", "N", "S",
