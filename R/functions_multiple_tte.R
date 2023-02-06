30806444bb43e94b15228cfd1df038eb8d91baaf
Johannes Cepicka <j36.cepicka@gmail.com>
2023-02-06 16:47:19 +0100

    correcting errors in functions_multiple_tte

--- R/functions_multiple_tte.R
+++ R/functions_multiple_tte.R
@@ -83,7 +83,7 @@ pgo_multiple_tte<-function(HRgo,n2,hr1,hr2,id1,id2,fixed,rho){
  if(fixed) {
       return(integrate(function(x){
        sapply(x,function(x)
-        fmax(x,hr1,hr2,sqrt(var1),sqrt(var2),rho))
+        fmax(x,-log(hr1),-log(hr2),sqrt(var1),sqrt(var2),rho))
         },-log(HRgo),Inf)$value)
     }
   
@@ -93,7 +93,8 @@ pgo_multiple_tte<-function(HRgo,n2,hr1,hr2,id1,id2,fixed,rho){
          integrate(function(v){
             sapply(v,function(v){
               integrate(function(x){sapply(x,function(x){
-              (fmax(x,u,v,sqrt(var1),sqrt(var2),rho))*(dbivanorm(u,v,-log(hr1),-log(hr2),vartrue1,vartrue2,rho))
+              (fmax(x,u,v,sqrt(var1),sqrt(var2),rho))
+                *(dbivanorm(u,v,-log(hr1),-log(hr2),vartrue1,vartrue2,rho))
             })
           },-log(HRgo),Inf)$value
         })
@@ -143,7 +144,8 @@ Ess_multiple_tte<-function(HRgo,n2,alpha,beta,hr1,hr2,id1,id2,fixed,rho){
  if(fixed)  {
          return(integrate(function(x){
           sapply(x,function(x){
-            ((4*(qnorm(1 - alpha) + qnorm(1 - beta))^2)/x^2)*fmax(x,-log(hr1),-log(hr2),sqrt(var1),sqrt(var2),rho)
+            ((4*(qnorm(1 - alpha) + qnorm(1 - beta))^2)/x^2)
+            *fmax(x,-log(hr1),-log(hr2),sqrt(var1),sqrt(var2),rho)
             })
         },-log(HRgo),Inf)$value)
       }
@@ -155,7 +157,9 @@ Ess_multiple_tte<-function(HRgo,n2,alpha,beta,hr1,hr2,id1,id2,fixed,rho){
                 sapply(v,function(v){
                   integrate(function(x){
                     sapply(x,function(x){
-                      (((4*(qnorm(1 - alpha) + qnorm(1 - beta))^2)/x^2)*fmax(x,u,v,sqrt(var1),sqrt(var2),rho))*(dbivanorm(u,v,-log(hr1),-log(hr2),vartrue1,vartrue2,rho))
+                      (((4*(qnorm(1 - alpha) + qnorm(1 - beta))^2)/x^2)
+                       *fmax(x,u,v,sqrt(var1),sqrt(var2),rho))
+                      *(dbivanorm(u,v,-log(hr1),-log(hr2),vartrue1,vartrue2,rho))
                     })
                   },-log(HRgo),Inf)$value #divide value by pgo to get E(e3|GO)
                 })
@@ -204,7 +208,8 @@ else {
       sapply(u,function(u){
         integrate(function(v){
           sapply(v,function(v){
-            pnorm(0,mean=(u-v),sd=sqrt(var1+var2-2*rho*sqrt(var1)*sqrt(var2)),lower.tail=FALSE)*(dbivanorm(u,v,-log(hr1),-log(hr2),vartrue1,vartrue2,rho))
+            pnorm(0,mean=(u-v),sd=sqrt(var1+var2-2*rho*sqrt(var1)*sqrt(var2)),lower.tail=FALSE)
+            *(dbivanorm(u,v,-log(hr1),-log(hr2),vartrue1,vartrue2,rho))
           })
         },-Inf,Inf)$value
       })
@@ -265,7 +270,8 @@ EPsProg_multiple_tte<-function(HRgo,n2,alpha,beta,ec,hr1,hr2,id1,id2,step1,step2
           sapply(x,function(x){ 
             integrate(function(y){ 
               sapply(y,function(y){
-                fmax(y,-log(hr1)/sqrt((x^2/c)*(ec/hr[1])),-log(hr2)/sqrt((x^2/c)*(ec/hr[2])),1,1,rho)*fmax(x,-log(hr1),-log(hr2),sqrt(var1),sqrt(var2),rho)
+                fmax(y,-log(hr1)/sqrt((x^2/c)*(ec/hr[1])),-log(hr2)/sqrt((x^2/c)*(ec/hr[2])),1,1,rho)
+                *fmax(x,-log(hr1),-log(hr2),sqrt(var1),sqrt(var2),rho)
               })
             },qnorm(1-alpha)-log(step1)/sqrt((x^2/c)),qnorm(1-alpha)-log(step2)/sqrt((x^2/c)))$value
           })
@@ -284,15 +290,16 @@ EPsProg_multiple_tte<-function(HRgo,n2,alpha,beta,ec,hr1,hr2,id1,id2,step1,step2
                   integrate(function(y){ 
                     sapply(y,function(y){
                       (fmax(y,-log(hr1)/sqrt((x^2/c)*(hr[ec]/hr[1])),-log(hr2)/sqrt((x^2/c)*(hr[ec]/hr[2])),1,1,rho)
-                       *fmax(x,-log(hr1),-log(hr2),sqrt(var1),sqrt(var2),rho)*dbivanorm(u,v,hr1,hr2,vartrue1,vartrue2,rho))
+                       *fmax(x,-log(hr1),-log(hr2),sqrt(var1),sqrt(var2),rho)
+                       *dbivanorm(u,v,-log(hr1),-log(hr2),vartrue1,vartrue2,rho))
                     })
                   },qnorm(1-alpha)-log(step1)/sqrt((x^2/c)),qnorm(1-alpha)-log(step2)/sqrt((x^2/c)))$value
                 })
               },-log(HRgo),Inf)$value
             })
-          },-1000,1000)$value
+          },-Inf,Inf)$value
         })
-      },-1000,1000)$value)
+      },-Inf,Inf)$value)
      
    }
 }
